72c677c76c930a4b80f368d07e444622c00a437e
NOZ <71051811+tnehf18@users.noreply.github.com>
2021-08-19 03:04:39 +0900

    Add files via upload

--- java/questions/ex_1st/ex_1st_05/no_05/Solution.java
+++ java/questions/ex_1st/ex_1st_05/no_05/Solution.java
@@ -13,21 +13,21 @@ class Solution {
     
     // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
     public static void main(String[] args) {
-	    Solution sol = new Solution();
-	      
-	    int[] enemies1 = {1, 4, 3};
-	    int[] armies1 = {1, 3};
-	    int ret1 = sol.solution(enemies1, armies1);
-	    
-	    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-	    System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
-	    
-	    int[] enemies2 = {1, 1, 1};
-	    int[] armies2 = {1, 2, 3, 4};
-	    int ret2 = sol.solution(enemies2, armies2);
-	    
-	    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-	    System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
+        Solution sol = new Solution();
+          
+        int[] enemies1 = {1, 4, 3};
+        int[] armies1 = {1, 3};
+        int ret1 = sol.solution(enemies1, armies1);
+        
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
+        
+        int[] enemies2 = {1, 1, 1};
+        int[] armies2 = {1, 2, 3, 4};
+        int ret2 = sol.solution(enemies2, armies2);
+        
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
     }
  
 }
